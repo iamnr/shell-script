db49fe65f87da842105fd8d9b73e15db9c03b7d5
iamnr <avuthunaveenreddy@gmail.com>
2023-10-25 16:39:37 +0530

    Commit

--- 09-functions.sh
+++ 09-functions.sh
@@ -1,36 +1,42 @@
-#!bin/bash
+#!/bin/bash
+
+# our program goal is to install mysql
 
 DATE=$(date +%F)
 SCRIPT_NAME=$0
-PATH=/home/centos/LogsFolder
-LOGFILE=$PATH/$SCRIPT_NAME-$DATE.log
+LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
 
-#echo "$LOGFILE"
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
+# this function should validate the previous command and inform user it is success or failure
 VALIDATE(){
-
-    if [ $1 -ne 0]
-    then    
-        echo "Failure"
+    #$1 --> it will receive the argument1
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILURE $N"
         exit 1
     else
-        echo -e "$2 Sucess"
+        echo -e "$2 ... $G SUCCESS $N"
     fi
 }
-
 
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-    echo "Run with root access"
+    echo "ERROR:: Please run this script with root access"
     exit 1
+# else
+#     echo "INFO:: You are root user"
 fi
 
+# it is our responsibility again to check installation is success or not
 yum install mysql -y &>>$LOGFILE
 
-VALIDATE $? "Installing Mysql"
+VALIDATE $? "Installing MySQL"
 
+yum install posddtfix -y &>>$LOGFILE
 
-
-
+VALIDATE $? "Installing postfix"
