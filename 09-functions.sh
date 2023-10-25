ece9f44eea12bed0cc527780b29ef2a2cc2aa366
iamnr <avuthunaveenreddy@gmail.com>
2023-10-25 17:19:14 +0530

    Commit

--- 09-functions.sh
+++ 09-functions.sh
@@ -1,12 +1,5 @@
 #!bin/bash
 
-DATE=$(date +%F)
-SCRIPT_NAME=$0
-PATH=/home/centos/shell-script/logsfolder
-LOGFILE=$PATH/$SCRIPT_NAME-$DATE.log
-
-#echo "$LOGFILE"
-
 yum install mysql -y 
 
 echo "$?"
