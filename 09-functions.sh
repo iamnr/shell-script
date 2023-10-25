a62f5de31b34046c0bad415233635d363bb1e7d2
iamnr <avuthunaveenreddy@gmail.com>
2023-10-25 16:25:51 +0530

    Commit

--- 09-functions.sh
+++ 09-functions.sh
@@ -1,5 +1,22 @@
 #!bin/bash
 
+DATE=$(date +%F)
+SCRIPT_NAME=$0
+PATH=/home/centos/LogsFolder
+LOGFILE=$PATH/$SCRIPT_NAME-$DATE.log
+
+#echo "$LOGFILE"
+
+VALIDATE(){
+
+    if [ $1 -ne 0]
+    then    
+        echo "Failure"
+        exit 1
+    else
+        echo "Sucess"
+    fi
+}
 
 
 USERID=$(id -u)
@@ -11,5 +28,11 @@ then
 else
     echo "Sucess"
 fi
+
+yum install mysql -y &>>$LOGFILE
+
+VALIDATE $? "Installing Mysql"
+
+
 
 
